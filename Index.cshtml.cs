1004b21b0d32911d301d6ae8a47f800f17f41bf4
dpmanGH <132672313+dpmanGH@users.noreply.github.com>
2023-05-05 18:37:12 +0600

    Add files via upload

--- Index.cshtml.cs
+++ Index.cshtml.cs
@@ -1,20 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data.SqlClient;
 
-namespace MyStore.Pages
+namespace MyStore.Pages.Clients
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
+        public List<ClientInfo> listClients = new List<ClientInfo>();
         public void OnGet()
         {
+            try
+            {
+                String connectionString = "Data Source=.\\sqlexpress;Initial Catalog=mystore;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    String sql = "SELECT * FROM clients";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using(SqlDataReader reader = command.ExecuteReader())
+                        { 
+                            while(reader.Read())
+                            {
+                                ClientInfo info = new ClientInfo();
+                                info.id = "" + reader.GetInt32(0);
+                                info.name = reader.GetString(1);
+                                info.email = reader.GetString(2);
+                                info.phone = reader.GetString(3);
+                                info.address = reader.GetString(4);
+                                info.created_at = reader.GetDateTime(5).ToString();
 
+                                listClients.Add(info);
+                            }
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+        }
+        public class ClientInfo
+        {
+            public string id;
+            public string name;
+            public string email;
+            public string phone;
+            public string address;
+            public string created_at;
         }
     }
 }
